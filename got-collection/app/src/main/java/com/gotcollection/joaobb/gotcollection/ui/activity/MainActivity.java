b51a1fc1db8d6c684bfa3cfa123d7c6260d8e831
João Henrique <jhbbortolotto@gmail.com>
2019-01-24 11:20:32 -0200

    feat: Activity data binding.

--- got-collection/app/src/main/java/com/gotcollection/joaobb/gotcollection/ui/activity/MainActivity.java
+++ got-collection/app/src/main/java/com/gotcollection/joaobb/gotcollection/ui/activity/MainActivity.java
@@ -1,15 +1,40 @@
 package com.gotcollection.joaobb.gotcollection.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.gotcollection.joaobb.gotcollection.R;
+import com.gotcollection.joaobb.gotcollection.databinding.ActivityMainBinding;
+import com.gotcollection.joaobb.gotcollection.db.entity.CharacterEntity;
+import com.gotcollection.joaobb.gotcollection.viewmodel.MainViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    ActivityMainBinding mBinding;
+    MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mViewModel = new MainViewModel(getApplication());
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBinding.setViewModel(mViewModel);
+
+        mViewModel.loadCharacters().observe(this, new Observer<List<CharacterEntity>>() {
+            @Override
+            public void onChanged(@Nullable List<CharacterEntity> characterEntities) {
+                Log.d(TAG, "onChanged() returned: " + characterEntities.size());
+            }
+        });
     }
 }

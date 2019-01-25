e1f1eea85f11a5aa3010b4e9bdb28c4e7121b9a7
João Henrique <jhbbortolotto@gmail.com>
2019-01-25 16:44:16 -0200

    feat: View Page test.

--- got-collection/app/src/main/java/com/gotcollection/joaobb/gotcollection/ui/activity/MainActivity.java
+++ got-collection/app/src/main/java/com/gotcollection/joaobb/gotcollection/ui/activity/MainActivity.java
@@ -2,11 +2,19 @@ package com.gotcollection.joaobb.gotcollection.ui.activity;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.gotcollection.joaobb.gotcollection.R;
 import com.gotcollection.joaobb.gotcollection.databinding.ActivityMainBinding;
+import com.gotcollection.joaobb.gotcollection.ui.fragment.CharacterList2Fragment;
+import com.gotcollection.joaobb.gotcollection.ui.fragment.CharacterListFragment;
 import com.gotcollection.joaobb.gotcollection.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mBinding;
     MainViewModel mViewModel;
+
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 5;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +51,50 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.main);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.viewPager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Log.d(TAG, "getItem() called with: position = [" + position + "]");
+            switch (position) {
+                case 0:
+                    return new CharacterListFragment();
+                case 1:
+                    return new CharacterList2Fragment();
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }

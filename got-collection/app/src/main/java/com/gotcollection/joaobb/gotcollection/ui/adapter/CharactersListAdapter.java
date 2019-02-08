75b1b6fceb6285094d07323b02ab430cdd95b209
João Henrique <jhbbortolotto@gmail.com>
2019-02-08 11:59:25 -0200

    feat: Animation on recycler view item.

--- got-collection/app/src/main/java/com/gotcollection/joaobb/gotcollection/ui/adapter/CharactersListAdapter.java
+++ got-collection/app/src/main/java/com/gotcollection/joaobb/gotcollection/ui/adapter/CharactersListAdapter.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.gotcollection.joaobb.gotcollection.R;
 import com.gotcollection.joaobb.gotcollection.databinding.CharacterCardBinding;
@@ -22,6 +24,7 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
     private Context mContext;
 
     private final CardClickListener onCardClick;
+    private int lastPosition = -1;
 
     public interface CardClickListener {
         void onItemClick(Pair<CharacterEntity, View> characterEntityViewPair);
@@ -67,7 +70,18 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
         });
 
         charactersViewHolder.bind(character);
+
+        setAnimation(charactersViewHolder.itemView, i);
     }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(mContext, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
+    }
+
 
     @Override
     public int getItemCount() {
